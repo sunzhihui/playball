b610966bff6c521b8fe22c41aa255b25a141e2ee
1293971940@qq.com <hao023340>
2019-07-09 17:37:09 +0800

    绑定手机号，我的钱包

--- app/api/error/Common.php
+++ app/api/error/Common.php
@@ -15,16 +15,23 @@ class Common
 {
 
     public static $passwordError            = [API_CODE_NAME => 1010001, API_MSG_NAME => '登录密码错误'];
-    public static $codewordError            = [API_CODE_NAME => 1010001, API_MSG_NAME => '验证码错误'];
 
-    public static $phoneCodeEmpty           = [API_CODE_NAME => 1010002, API_MSG_NAME => '手机号或验证码不能为空'];
+    public static $codewordError            = [API_CODE_NAME => 1010002, API_MSG_NAME => '验证码错误'];
 
-    public static $registerFail             = [API_CODE_NAME => 1010003, API_MSG_NAME => '注册失败'];
+    public static $phoneError            = [API_CODE_NAME => 1010003, API_MSG_NAME => '手机号格式错误'];
 
-    public static $phoneFail                = [API_CODE_NAME => 1010003, API_MSG_NAME => '手机号已使用'];
+    public static $phoneExist            = [API_CODE_NAME => 1010004, API_MSG_NAME => '手机号已存在'];
 
-    public static $oldOrNewPassword         = [API_CODE_NAME => 1010004, API_MSG_NAME => '旧密码或新密码不能为空'];
+    public static $phoneBindError            = [API_CODE_NAME => 1010005, API_MSG_NAME => '已绑定手机号'];
 
-    public static $changePasswordFail       = [API_CODE_NAME => 1010005, API_MSG_NAME => '密码修改失败'];
+    public static $phoneCodeEmpty           = [API_CODE_NAME => 1010006, API_MSG_NAME => '手机号或验证码不能为空'];
+
+    public static $registerFail             = [API_CODE_NAME => 1010007, API_MSG_NAME => '注册失败'];
+
+    public static $phoneFail                = [API_CODE_NAME => 1010008, API_MSG_NAME => '手机号已使用'];
+
+    public static $setPhoneFail                = [API_CODE_NAME => 1010009, API_MSG_NAME => '手机号绑定失败'];
+
+
 
 }
--- app/api/validate/User.php
+++ app/api/validate/User.php
@@ -16,13 +16,14 @@ class User extends ApiBase
 
     // 验证提示
     protected $message = [
-        'phone.require'    => '手机号不能为空',
-        'code.require'    => '验证码不能为空',
+        'phone.require'=> '手机号不能为空',
+        'code.require' => '验证码不能为空',
     ];
 
     // 应用场景
     protected $scene = [
         'login'  =>  ['phone','code'],
+        'setPhone' => ['phone','code'],
     ];
 }
 
